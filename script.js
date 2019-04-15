a3e32985ea4df3f16fd2e95f7200d0f625b4e182
donatociao <ciao.donatociao@gmail.com>
2019-04-15 22:45:14 +0200

    ok

---
+++ script.js
@@ -0,0 +1,4 @@
+var nome = prompt('Mi dici il tuo nome?');
+var cognome = prompt('E il tuo cognome?');
+var colore = prompt('Quale è il tuo colore preferito?');
+document.getElementById('pass').innerHTML= nome + cognome + colore + 2019;
